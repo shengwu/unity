0179368380a2e8027551d88593f1e83722791832
Sheng Wu <pkernoobie@gmail.com>
2016-11-08 08:43:02 -0600

    UI - count and win text

--- Roll a Ball/Assets/scripts/PlayerController.cs
+++ Roll a Ball/Assets/scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -20,6 +27,8 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+			count += 1;
+			setCountText ();
 		}
 	}
 
@@ -30,5 +39,12 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
+	}
+
+	void setCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
 	}
 }
